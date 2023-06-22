29f253335df5e414d37a6a271a5c667ee7884629
Thinktan <tantao0675@hnu.edu.cn>
2023-06-22 12:54:39 +0800

    up from win

--- fastword2vec/negative_sampling_layer.py
+++ fastword2vec/negative_sampling_layer.py
@@ -2,10 +2,46 @@
 import sys
 sys.path.append('..')
 from common.np import *  # import numpy as np
-from common.layers import EmbeddingDot, SigmoidWithLoss
+from common.layers import Embedding, SigmoidWithLoss
 import collections
 
 # 负采样思想：将多分类问题转成二分类问题
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.params.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        '''前向传播
+
+        :param h: (N,D) 中间层神经元 D:神经元个数, W形状是：(w, D)
+        :param idx: (N,) 单词ID列表 mini-batch，从W(w,D)中取出N个得到(N,D)
+        :return: (N,)
+        '''
+        target_W = self.embed.forward(idx) # (N,D)
+        out = np.sum(target_W*h, axis=1) # (N,) 逐元素乘法操作 -> sum
+
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        '''
+
+        :param dout:  shape(1*N)
+        :return:
+        '''
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1) # (N,1)
+        # dout之前属于sum操作，因此广播回形状，基于乘法的反向传播逻辑，计算dtarget_W和dh
+        dtarget_W = dout * h # (N*1 -> N*D)x(N*D)=(N,D)
+        self.embed.backward(dtarget_W)
+        dh = dout*target_W #(N*1 -> N*D)x(N*D)=(N*D)
+
+        return dh
+
 
 class UnigramSampler:
     def __init__(self, corpus, power, sample_size):
