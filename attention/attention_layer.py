c26858ed90501e95c68a3e4ee57c0d2a092dc896
Thinktan <55999974+Thinktan@users.noreply.github.com>
2023-07-04 15:36:24 +0800

    up from pro

--- attention/attention_layer.py
+++ attention/attention_layer.py
@@ -30,3 +30,33 @@ class WeightSum:
 
         return dhs, da
 
+
+class AttentionWeight:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.softmax = Softmax()
+        self.cache = None
+
+    def forward(self, hs, h):
+        N, T, H = hs.shape
+
+        hr = h.reshape(N, 1, H).repeat(T, axis=1) # (N,T,H)
+        t = hs * hr # (N,T,H)
+        s = np.sum(t, axis=2) # (N,T)
+        a = self.softmax.forward(s) # (N,T)
+
+        self.cache = (hs, hr)
+        return a # (N,T)
+
+    def backward(self, da):
+        hs, hr = self.cache
+        N, T, H = hs.shape
+
+        ds = self.softmax.backward(da)
+        dt = ds.reshape(N, T, 1).repeat(H, axis=2) # (N,T,H)
+        dhs = dt * hr
+        dhr = dt * hs
+        dh = np.sum(dhr, axis=1)
+
+        return dhs, dh
+
